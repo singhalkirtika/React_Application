e512c01428a42e83e1e5c8c450c0b473d0753ae6
singhalkirtika <singhalkirtika28@gmail.com>
2018-10-20 22:48:00 +0530

    Assignment3

--- src/components/CommentForm.js
+++ src/components/CommentForm.js
@@ -1,6 +1,4 @@
-import React, { Component } from 'react';
-import { Card, CardBody, CardTitle, CardImg, CardText,  Breadcrumb, BreadcrumbItem  } from 'reactstrap';
-import { Link } from 'react-router-dom';
+import React, {Component} from 'react';
 import { Button, Row, Label, ModalBody, Modal, ModalHeader } from 'reactstrap';
 import { Control, LocalForm, Errors} from 'react-redux-form';
 
@@ -35,7 +33,7 @@ class CommentForm extends Component{
       <React.Fragment>
 
       <Button onClick={this.toggleModal} color="light" className="btn-outline-dark">
-        <span className="fa fa-pencil fa-lg"></span> Submit Comment</Button>
+        <span className="fa fa-edit fa-lg"></span> Submit Comment</Button>
 
         <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
           <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
@@ -81,77 +79,4 @@ class CommentForm extends Component{
   }
 }
 
-function RenderDish({dish}){
-    if(dish!=null){
-      return(
-        <Card>
-          <CardImg top src={dish.image} alt={dish.name}/>
-            <CardBody>
-              <CardTitle>{dish.name}</CardTitle>
-              <CardText>{dish.description}</CardText>
-          </CardBody>
-       </Card>
-      );
-    }
-    else{
-      return(
-        <div></div>
-      );
-    }
-
-}
-
-function RenderComments({comments}){
-     if(comments!=null){
-       return (
-         <ul className="list-unstyled">
-        {
-          comments.map( (comment) => {
-          return(
-            <li key={comment.id}>
-              <p>{comment.comment}</p>
-              <p>--{comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short',
-            day: '2-digit'}).format( new Date( Date.parse(comment.date)))}</p>
-            </li>
-            );
-          })
-        }
-        </ul>
-      );
-     }
-     else{
-       return(
-         <div></div>
-       );
-     }
-  }
-
-  const  DishDetail = (props) => {
-          return(
-          <div className="container">
-          <div className="row">
-              <Breadcrumb>
-                  <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
-                  <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
-              </Breadcrumb>
-              <div className="col-12">
-                  <h3>{props.dish.name}</h3>
-                  <hr />
-              </div>
-          </div>
-            <div className="row">
-             <div className="col-12 col-md-5 m-1">
-               <RenderDish dish={props.dish} />
-             </div>
-             <div className="col-12 col-md-5 m-1">
-               <h4>Comments</h4>
-                <RenderComments comments={props.comments}/>
-                <CommentForm/>
-             </div>
-            </div>
-          </div>
-          );
-    }
-
-
-export default DishDetail;
+export default CommentForm;
